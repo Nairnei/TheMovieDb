224a3dcfc4383ffa003d9971009ced0f0d87654a
Rodrigo Nairnei <ronairnei@gmail.com>
2021-11-13 17:24:02 -0300

    feat: create test api. close #2

---
+++ test/the_movie_db_api_test.dart
@@ -0,0 +1,57 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  var apiKey = "7e49e84a68cdbea10b67f65709d1bc64";
+  var baseUrl = "https://api.themoviedb.org";
+
+  var apiVersion = "3";
+  var page = 1;
+  var language = "pt-BR";
+
+  //--------------------- TheMovieDB API-----------------------------
+
+  ///list tendency movie with paginate and language example
+  test("listCategories", () async {
+    var query = "genre/movie/list";
+    var response = await Dio().get(
+        "$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language");
+    expect(response.statusCode, 200);
+  });
+
+  ///list movie on Theaters with paginate and language example
+  test("listCurrentTheatersMovie", () async {
+    var query = "movie/now_playing";
+    var response = await Dio().get(
+        '$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language');
+    expect(response.statusCode, 200);
+  });
+
+  ///discover movies with genres
+  test("listDiscoverGenres", () async {
+    var genreId = "16,28";
+    var query = "discover/movie";
+    var response = await Dio().get(
+        '$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language&with_genres=$genreId');
+    expect(response.statusCode, 200);
+  });
+
+  ///get movie details by id
+  test("getMovieDetailsByID", () async {
+    var query = "movie/438631";
+    var response =
+        await Dio().get('$baseUrl/$apiVersion/$query?api_key=$apiKey');
+    print(
+        '$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language');
+    expect(response.statusCode, 200);
+  });
+
+  ///search movie with querySearch
+  test("searchMovieWithQuery", () async {
+    var query = "search/movie";
+    var querySearch = "007";
+    var response = await Dio().get(
+        '$baseUrl/$apiVersion/$query?api_key=$apiKey&query=$querySearch&page=$page&language=$language');
+    expect(response.statusCode, 200);
+  });
+}
