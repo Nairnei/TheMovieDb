393b5599aa93f64f66507d33bb45b97036964f39
Rodrigo Nairnei <ronairnei@gmail.com>
2021-11-19 14:58:42 -0300

    remove async on interfaces. close #18

--- lib/interface/movie_interface.dart
+++ lib/interface/movie_interface.dart
@@ -2,17 +2,17 @@ import 'package:dio/dio.dart';
 
 abstract class InterfaceMovie {
   ///list moview genres
-  Future<Response?> listCategories() async {}
+  listCategories() {}
 
   ///list movies on Theater now
-  Future<Response?> listCurrentTheatersMovie() async {}
+  Future<Response?> listCurrentTheatersMovie();
 
   ///Discover movies with genres
-  Future<Response?> listDiscoverGenres(var sendData) async {}
+  Future<Response?> listDiscoverGenres(var sendData);
 
   ///get details of movie to show a more complete details screen
-  Future<Response?> getMovieDetailsByID(var sendData) async {}
+  Future<Response?> getMovieDetailsByID(var sendData);
 
   ///search movie by query
-  Future<Response?> searchMovieWithQuery(var sendData) async {}
+  Future<Response?> searchMovieWithQuery(var sendData);
 }
